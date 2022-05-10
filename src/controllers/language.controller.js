508a1e70466c1f5b6adc4eb1d142b98bd5a11917
BrandoAngulo <steven.sm.1992@gmai.com>
2022-05-10 17:34:00 -0500

    testeando las peticiones revisar peticion por params ?

--- src/controllers/language.controller.js
+++ src/controllers/language.controller.js
@@ -4,6 +4,20 @@ import { getConnection } from "../database/database";
 //await = nos permite indicar que debemos esperar a que finalice cierto bloque de codigo
 const getLanguages = async (req, res) => {
     try {
+        console.log(req.params);
+        const {id}=req.params;
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, name, programmers FROM language WHERE id = ?",id);//recibe el parametro id que hemos hecho con la const por parametro 
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+//obtener la peticion del lenguage por el parametro osea por medio de la url
+const getLanguage = async (req, res) => {
+    try {
         const connection = await getConnection();
         const result = await connection.query("SELECT id, name, programmers FROM language");
         res.json(result);
@@ -12,7 +26,27 @@ const getLanguages = async (req, res) => {
         res.send(error.message);
     }
 };
+const addLanguage = async (req, res) => {
+    try {
+        //creacion de constantes que tenemos en la db para asi hacer la inserccion de datos a la misma db 
+        const {name, programmers}=req.body; //con req.body quiere decir que enviamos la peticion por el cuerpo del codigo
+        //condicional si alguno de los campos se envian vacios
+        if (name === undefined || programmers === undefined) {
+            res.status(400).json({message: "Bad Request. Please fill all field. "});//solicitud mala. porfavor rellene todas los campos
+        }
+        //creacion de objeto para hacer la inserccion sin necesidad de digitar el query con tantos datos
+        const language = {name, programmers};
+        const connection = await getConnection();                                     
+        const result = await connection.query("INSERT INTO language SET ?",language);//en ves de values(datos para inserccion concatenados ) se pone SET ?, y el objeto con los valores para la inserccion que hay en la base ded atos
+        res.json({message: "Language added"});//respuesta cuando se agregue el lenguaje
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
 //exportar la funcion para poderla importar en cualquier otro lugar, se exporta mediante la llave methods
 export const methods = {
-    getLanguages
+    getLanguages,
+    getLanguage,
+    addLanguage
 };
--- src/routes/language.routes.js
+++ src/routes/language.routes.js
@@ -3,6 +3,12 @@ import { methods as languageController } from "../controllers/language.controlle
 
 const router = Router();
 
+//rutas 
+// ruta para obtener peticiones con get
 router.get("/", languageController.getLanguages);
+//ruta para obtener la peticion recibiendo parametros por url
+router.get("/:id", languageController.getLanguage);//la funcion seria getLanguage
+//ruta para enviar peticiones con post
+router.post("/", languageController.addLanguage);
 
 export default router; 
